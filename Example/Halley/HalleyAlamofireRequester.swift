c72e1ef8b3c2aa10cfccdbd17b177fc09e598f6c
Zoran <zoran.turk@infinum.hr>
2022-03-11 17:07:28 +0100

    Added subspec for URITemplate handling
    • Added some DefaultTemplateHanlder logic that can be used to resolve URITemplate logic

--- Example/Halley/HalleyAlamofireRequester.swift
+++ Example/Halley/HalleyAlamofireRequester.swift
@@ -20,7 +20,7 @@ class HALAlamofireRequester: RequesterInterface {
         print("[REQUEST]: \(url.absoluteString)")
         let headers = HTTPHeaders(
             [
-                "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MmVlZDFiNy1jMDcyLTRkZGQtOGFiNS01NDM2ZTg2ZDA1YjAiLCJpc3MiOiJkZXYuYmFja2VuZC5rYS5waGlsaXBzLmNvbSIsInVzZXJuYW1lIjoiNDJlZWQxYjctYzA3Mi00ZGRkLThhYjUtNTQzNmU4NmQwNWIwIiwiZXhwIjoxNjQ3MjgwMTkyLCJ0eXBlIjoiY29uc3VtZXIiLCJhdWQiOm51bGwsInN1YiI6IjQyZWVkMWI3LWMwNzItNGRkZC04YWI1LTU0MzZlODZkMDViMCJ9.iIjyFOQxfjoIp1182-5rGipTKeU9-TAXK62Tqwrns3IQGB5POFr8t0xuqQbZyL5kUT3KIDXQVwKY5cEV5kbG6cUru9_FTkhFViAx-ozdvIQVT0jWBvrcElf-Pw-Hdxi8uMWcSx6oqdFqkojgPPxMjhCtDEsBrRaTwSq5S8N06Z3OBY3k5mINdoBTcVwTYDjHqlg-juc32K7OyMUS8mlk6205zDwYXBAwcge_1GNO-edzyesxFSpk9WIHlqoVIs4pidtqnvTbYjJiogX5V4Vf_t5VA7HkzaQD4_M3ctIsXmJs7UXbnB76bs2QoJpCvJyR2EAc1OF19vMfdfpTUdEjSQ",
+                "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MmVlZDFiNy1jMDcyLTRkZGQtOGFiNS01NDM2ZTg2ZDA1YjAiLCJpc3MiOiJkZXYuYmFja2VuZC5rYS5waGlsaXBzLmNvbSIsInVzZXJuYW1lIjoiNDJlZWQxYjctYzA3Mi00ZGRkLThhYjUtNTQzNmU4NmQwNWIwIiwiZXhwIjoxNjQ3NTIyMzU3LCJ0eXBlIjoiY29uc3VtZXIiLCJhdWQiOm51bGwsInN1YiI6IjQyZWVkMWI3LWMwNzItNGRkZC04YWI1LTU0MzZlODZkMDViMCJ9.OhTPZP7eU_Jkl2cGK7nxnFmI8SWZWccgM2gxwpDkKwl9P9UslHN2rg1Hkm5LY3yyHrhBdzyUtuKFtiLR6jbp-QBVTcA7DjwqOYuxCpT-lWPMN7wNrvaRfPldUu3UlOpKMKN4jbGw0VcdJRiRPMUbaozyyvhzTvfhrWcna6P2sXiBl4dk2uz9eaqliHuU6dpozw1UIIw6_LFlak9kIB1g27CkK0Yf14TD17ibUF6KB6zUmyUoDJv370qOtEtvzIyYIWBTcDghVuRnuZd4MabmKWchPVaMO36j9-KuOOVqZIkuCHcfznRkB6The3Z29UHm0xGWYXQPro8jcPh1v63AuA",
                 "Accept-Language": "de-DE",
                 "accept": "application/vnd.oneka.v2.0+json"
             ]
---
+++ Halley/Classes/URITemplate/TemplateLinkResolver.swift
@@ -0,0 +1,76 @@
+//
+//  TemplateLinkResolver.swift
+//  Halley_Example
+//
+//  Created by Zoran Turk on 25.02.2022..
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import URITemplate
+
+public protocol TemplateHandler {
+    func resolveTemplate(for link: Link) -> String
+}
+
+public class TemplateLinkResolver: LinkResolver {
+    let includeParameters: [String: [URLQueryItem]]
+    let templateHandler: TemplateHandler
+
+    public init(
+        parameters: [String: [URLQueryItem]],
+        templateHandler: TemplateHandler = DefaultTemplateHandler.shared
+    ) {
+        self.includeParameters = parameters
+        self.templateHandler = templateHandler
+    }
+
+    public func resolveLink(_ link: Link, relationshipPath: String?) throws -> URL {
+        let resolvedString = templateHandler.resolveTemplate(for: link)
+
+        var urlComponent = try URLComponents(string: resolvedString) ?? throwError(HalleyKit.Error.cantResolveURLFromLink(link: link))
+        guard let parent = relationshipPath else { return try urlComponent.asURL() }
+        let parameters = includeParameters[parent] ?? []
+        urlComponent.queryItems = (urlComponent.queryItems ?? []) + parameters
+        return try urlComponent.asURL()
+    }
+}
+
+public class DefaultTemplateHandler: TemplateHandler {
+
+    public static let shared = DefaultTemplateHandler()
+
+    private var templateValues: [String: () -> String?] = [:]
+
+    private init() { /* Singleton pattern */ }
+
+    public func resolveTemplate(for link: Link) -> String {
+        guard link.templated == true else { return link.href }
+
+        var expandValues = [String: String]()
+        for item in templateValues {
+            if let value = item.value() {
+                expandValues.updateValue(value, forKey: item.key)
+            }
+        }
+
+        let template = URITemplate(template: link.href)
+        return template.expand(expandValues)
+    }
+
+    public func updateTamplate(for key: String, value: @escaping () -> String?) {
+        templateValues.updateValue(value, forKey: key)
+    }
+
+    public func removeTemplate(for key: String) {
+        templateValues.removeValue(forKey: key)
+    }
+}
+
+extension URLComponents {
+
+    func asURL() throws -> URL {
+        guard let url = url else { throw HalleyKit.Error.cantResolveURLFromString(string: string)}
+        return url
+    }
+}
