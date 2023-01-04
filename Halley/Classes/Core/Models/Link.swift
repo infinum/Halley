b303e6cd43fa7f1e9486ce160b642d26fcdbdfde
Filip Gulan <gulan.filip@gmail.com>
2023-01-04 13:26:17 +0100

    Add optional Link parameters

--- Halley/Classes/Core/Models/Link.swift
+++ Halley/Classes/Core/Models/Link.swift
@@ -3,14 +3,42 @@ import Foundation
 public struct Link: Codable {
     public let href: String
     public let templated: Bool?
+    public let type: String?
+    public let deprecation: String?
+    public let name: String?
+    public let profile: String?
+    public let title: String?
+    public let hreflang: String?
 
-    enum CodingKeys: String, CodingKey {
+
+    public enum CodingKeys: String, CodingKey {
         case href
         case templated
+        case type
+        case deprecation
+        case name
+        case profile
+        case title
+        case hreflang
     }
 
-    public init(href: String, templated: Bool?) {
+    public init(
+        href: String,
+        templated: Bool? = nil,
+        type: String? = nil,
+        deprecation: String? = nil,
+        name: String? = nil,
+        profile: String? = nil,
+        title: String? = nil,
+        hreflang: String? = nil
+    ) {
         self.href = href
         self.templated = templated
+        self.type = type
+        self.deprecation = deprecation
+        self.name = name
+        self.profile = profile
+        self.title = title
+        self.hreflang = hreflang
     }
 }
