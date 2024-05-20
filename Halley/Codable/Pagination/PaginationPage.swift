1480fb453d3d8240abce779980a92d5a3f0c25b1
Zoran <zoran.turk@infinum.hr>
2024-05-20 12:17:57 +0200

    Adds default _links property to PaginationPage resource
    • Odl _links property has been renamed to _pageLinks and is type of PageLink
    • New _links property is now type of Links to be consistent with other resources
    • Enables support for additional relationship links in the page resource, not just page links as before

--- Halley/Codable/Pagination/PaginationPage.swift
+++ Halley/Codable/Pagination/PaginationPage.swift
@@ -6,7 +6,8 @@ public class PaginationPage<T: Codable>: Codable {
 
     public let resources: [T]?
     public let metadata: PageMetadata
-    public let _links: PageLinks
+    public let _links: Links?
+    public let _pageLinks: PageLinks
 
     public enum CodingKeys: String, CodingKey {
         case resources = "item"
@@ -14,23 +15,33 @@ public class PaginationPage<T: Codable>: Codable {
         case _links
     }
 
-    public init(links: PageLinks, resources: [T]?, metadata: PageMetadata) {
-        _links = links
+    public required init(from decoder: Decoder) throws {
+        let container: KeyedDecodingContainer<PaginationPage<T>.CodingKeys> = try decoder.container(keyedBy: PaginationPage<T>.CodingKeys.self)
+        self.resources = try container.decodeIfPresent([T].self, forKey: PaginationPage<T>.CodingKeys.resources)
+        self.metadata = try container.decode(PageMetadata.self, forKey: PaginationPage<T>.CodingKeys.metadata)
+        self._links = try container.decode(Links.self, forKey: PaginationPage<T>.CodingKeys._links)
+        self._pageLinks = try container.decode(PageLinks.self, forKey: PaginationPage<T>.CodingKeys._links)
+    }
+
+    public init(pageLinks: PageLinks, resources: [T]?, metadata: PageMetadata, links: Links?) {
+        _pageLinks = pageLinks
         self.resources = resources
         self.metadata = metadata
+        _links = links
     }
 
     public static func empty() -> PaginationPage {
-        return PaginationPage(links: .empty(), resources: [], metadata: .empty())
+        return PaginationPage(pageLinks: .empty(), resources: [], metadata: .empty(), links: nil)
     }
 
     public func mapResources<New>(
         _ transform: (T) -> New
     ) -> PaginationPage<New> {
         return .init(
-            links: _links,
+            pageLinks: _pageLinks,
             resources: resources?.map(transform),
-            metadata: metadata
+            metadata: metadata,
+            links: _links
         )
     }
 }
