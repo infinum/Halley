7319d3588384b715d910c37099ae1ef4833cd9d7
Filip Gulan <gulan.filip@gmail.com>
2025-09-12 14:15:25 +0200

    Migrate macros test to Swift Testing

--- MacroTests/CodingKeysMacroTests.swift
+++ MacroTests/CodingKeysMacroTests.swift
@@ -1,18 +1,20 @@
-import XCTest
 import SwiftSyntax
+import SwiftSyntaxMacrosGenericTestSupport
 import SwiftSyntaxMacros
-import SwiftSyntaxMacrosTestSupport
+import SwiftSyntaxMacroExpansion
+import Testing
 @testable import HalleyMacro
 @testable import HalleyMacroPlugin
 
-final class CodingKeysMacroTests: XCTestCase {
+final class CodingKeysMacroTests {
     let macros: [String: Macro.Type] = [
         "HalleyModel": HalleyModelMacro.self,
         "HalleyCodingKey": HalleyCodingKeyMacro.self
     ]
 
+    @Test("Internal struct")
     func testInternalStruct() throws {
-        assertMacroExpansion(
+        assertMacroExpansionWithSwiftTesting(
             """
             protocol IncludeKey {
             }
@@ -49,8 +51,9 @@ final class CodingKeysMacroTests: XCTestCase {
         )
     }
 
+    @Test("Public struct")
     func testPublicStruct() throws {
-        assertMacroExpansion(
+        assertMacroExpansionWithSwiftTesting(
             """
             protocol IncludeKey {
             }
@@ -89,4 +92,37 @@ final class CodingKeysMacroTests: XCTestCase {
             macros: macros
         )
     }
+}
+
+func assertMacroExpansionWithSwiftTesting(
+    _ originalSource: String,
+    expandedSource expectedExpandedSource: String,
+    diagnostics: [DiagnosticSpec] = [],
+    macros: [String: any Macro.Type],
+    applyFixIts: [String]? = nil,
+    fixedSource expectedFixedSource: String? = nil,
+    testModuleName: String = "TestModule",
+    testFileName: String = "test.swift",
+    indentationWidth: Trivia = .spaces(4),
+    sourceLocation: Testing.SourceLocation = SourceLocation(fileID: #fileID, filePath: #filePath, line: #line, column: #column)
+) {
+    let macroSpecs = macros.mapValues { MacroSpec(type: $0) }
+    SwiftSyntaxMacrosGenericTestSupport.assertMacroExpansion(
+        originalSource,
+        expandedSource: expectedExpandedSource,
+        diagnostics: diagnostics,
+        macroSpecs: macroSpecs,
+        applyFixIts: applyFixIts,
+        fixedSource: expectedFixedSource,
+        testModuleName: testModuleName,
+        testFileName: testFileName,
+        indentationWidth: indentationWidth,
+        failureHandler: {
+            #expect(Bool(false), .init(stringLiteral: $0.message), sourceLocation: sourceLocation)
+        },
+        fileID: "", // Not used in the failure handler
+        filePath: "", // Not used in the failure handler
+        line: UInt(sourceLocation.line),
+        column: 0 // Not used in the failure handler
+    )
 }
